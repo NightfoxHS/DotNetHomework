ad16be70c5f906400a81cf495a265eeb3192f1ce
renranxie <renranxie@hotmail.com>
2021-02-28 19:57:01 +0800

    Homework1

---
+++ Homework1/Homework1_Console/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Homework1_Console
+{
+    class Calculate
+    {
+        private string str;
+        private float num1, num2;
+        private char symbol;
+        public Calculate(string str)
+        {
+            this.str = str;
+        }
+        public void setStr(string str)
+        {
+            this.str = str;
+        }
+        public string getStr()
+        {
+            return this.str;
+        }
+        public bool getResult(ref float result)
+        {
+            bool flag = false;
+            char[] exp_set = { '+', '-', '*', '/', '%' };
+            foreach(char x in exp_set)
+            {
+                if (str.Contains(x))
+                {
+                    string[] exp = this.str.Split(x, 2);
+                    this.num1 = float.Parse(exp[0]);
+                    this.num2 = float.Parse(exp[1]);
+                    this.symbol = x;
+                    flag = true;
+                }
+            }
+            switch (this.symbol)
+            {
+                case '+':
+                    result= num1 + num2;
+                    break;
+                case '-':
+                    result= num1 - num2;
+                    break;
+                case '*':
+                    result= num1 * num2;
+                    break;
+                case '/':
+                    result= num1 / num2;
+                    break;
+                case '%':
+                    result= num1 % num2;
+                    break;
+                default:
+                    return flag;
+            }
+            return flag;
+        }
+        static void Main(string[] args)
+        {
+            Calculate Calculator = new Calculate("");
+            bool flag = true;
+            float result = 0;
+            do
+            {
+                Console.WriteLine("Enter your expression:(+-*/%,'q' to quit)");
+                Calculator.setStr(Console.ReadLine());
+                if (string.Equals(Calculator.getStr(), "q"))
+                    flag = false;
+                if (flag)
+                    if (Calculator.getResult(ref result))
+                        Console.WriteLine(result);
+            } while (flag);
+        }
+    }
+}
---
+++ Homework1/Homework1_WPF/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Homework1_WPF
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            exp_ComboBox.SelectedIndex = 0;
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            float res = 0;
+            ComboBoxItem sel_item = (ComboBoxItem)exp_ComboBox.SelectedItem;
+            try
+            {
+                switch (Int32.Parse(sel_item.Tag.ToString()))
+                {
+                    case 0:
+                        res = float.Parse(num1_TextBox.Text) + float.Parse(num2_TextBox.Text);
+                        break;
+                    case 1:
+                        res = float.Parse(num1_TextBox.Text) - float.Parse(num2_TextBox.Text);
+                        break;
+                    case 2:
+                        res = float.Parse(num1_TextBox.Text) * float.Parse(num2_TextBox.Text);
+                        break;
+                    case 3:
+                        res = float.Parse(num1_TextBox.Text) / float.Parse(num2_TextBox.Text);
+                        break;
+                    case 4:
+                        res = float.Parse(num1_TextBox.Text) % float.Parse(num2_TextBox.Text);
+                        break;
+                    default:
+                        break;
+                }
+                res_Textbox.Text = res.ToString(); 
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Please enter the correct number.");
+            }
+        }
+    }
+}
