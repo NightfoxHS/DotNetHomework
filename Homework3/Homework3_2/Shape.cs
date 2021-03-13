3865b118579b3d01c88e721d4e8449c7f499fd85
renranxie <renranxie@hotmail.com>
2021-03-13 13:07:41 +0800

    Commit Homework3

---
+++ Homework3/Homework3_2/Shape.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Numerics;
+
+namespace Homework3_1
+{
+    /* 
+     * Shape的子类通过2或3个点来定义一个图形
+     * 当这2个3个点不符合相关图形定义（如：使得正方形长宽不相等）时，该图形的width/height/side等字段将不会被赋值（即，使用初始值0）
+     * 其面积也不会参与最后的总面积计算
+     */
+    interface ILegal
+    {
+        bool IsLegal();
+    }
+
+    public abstract class Shape : ILegal
+    {
+        public abstract float Area { get; }
+        public abstract bool IsLegal();
+    }
+
+    public class Triangle : Shape
+    {
+        private Vector2[] dot = new Vector2[3];
+        private float width;
+        private float height;
+
+        public Triangle(Vector2 dot1, Vector2 dot2, Vector2 dot3)
+        {
+            dot[0] = dot1; dot[1] = dot2; dot[2] = dot3;
+
+            if (IsLegal())
+            {
+                Vector2 vector1 = dot[0] - dot[1];
+                Vector2 vector2 = dot[2] - dot[1];
+                Vector2 vHeight = vector1 - vector2;
+                this.width = vector1.Length();
+                this.height = vHeight.Length();
+            }
+        }
+
+        public Triangle(float x1, float y1, float x2, float y2, float x3, float y3)
+        {
+            dot[0].X = x1; dot[0].Y = y1;
+            dot[1].X = x2; dot[1].Y = y2;
+            dot[2].X = x3; dot[2].Y = y3;
+            if (IsLegal())
+            {
+                Vector2 vector1 = dot[0] - dot[1];
+                Vector2 vector2 = dot[2] - dot[1];
+                Vector2 vHeight = vector1 - vector2;
+                this.width = vector1.Length();
+                this.height = vHeight.Length();
+            }
+            else
+                throw new Exception("The shape is illegal.");
+        }
+
+        override public bool IsLegal()
+        {
+            if (dot[0] == dot[1] || dot[1] == dot[2] || dot[2] == dot[0])
+                return false;
+            else
+                return true;
+        }
+
+        override public float Area
+        {
+            get => width * height / 2;
+        }
+    }
+
+    public class Rectangle : Shape
+    {
+        private Vector2[] dot = new Vector2[2];
+        private float width;
+        private float height;
+
+        public Rectangle(Vector2 dot1, Vector2 dot2)
+        {
+            dot[0] = dot1; dot[1] = dot2;
+
+            if (IsLegal())
+            {
+                width = Math.Abs(dot[0].X - dot[1].X);
+                height = Math.Abs(dot[0].Y - dot[1].Y);
+            }
+        }
+
+        public Rectangle(float x1, float y1, float x2, float y2)
+        {
+            dot[0].X = x1; dot[0].Y = y1;
+            dot[1].X = x2; dot[1].Y = y2;
+            if (IsLegal())
+            {
+                width = Math.Abs(dot[0].X - dot[1].X);
+                height = Math.Abs(dot[0].Y - dot[1].Y);
+            }
+            else
+                throw new Exception("The shape is illegal.");
+        }
+
+        override public bool IsLegal()
+        {
+            if (dot[0] == dot[1])
+                return false;
+            else
+                return true;
+        }
+
+        override public float Area
+        {
+            get => width * height;
+        }
+    }
+
+    public class Square : Shape
+    {
+        private Vector2[] dot = new Vector2[2];
+        private float side;
+
+        public Square(Vector2 dot1, Vector2 dot2)
+        {
+            dot[0] = dot1; dot[1] = dot2;
+
+            if (IsLegal())
+                side = Math.Abs(dot[0].X - dot[1].X);
+        }
+
+        public Square(float x1, float y1, float x2, float y2)
+        {
+            dot[0].X = x1; dot[0].Y = y1;
+            dot[1].X = x2; dot[1].Y = y2;
+            if (IsLegal())
+                side = Math.Abs(dot[0].X - dot[1].X);
+            else
+                throw new Exception("The shape is illegal.");
+        }
+
+        override public bool IsLegal()
+        {
+            if (dot[0] == dot[1] || Math.Abs(dot[0].X - dot[1].X) != Math.Abs(dot[0].Y - dot[1].Y))
+                return false;
+            else
+                return true;
+        }
+
+        override public float Area
+        {
+            get => side * side;
+        }
+    }
+}
---
+++ Homework3/Homework3_2/ShapeFactory.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Numerics;
+using System.Collections.Generic;
+using Homework3_1;
+
+namespace Homework3_2
+{
+    enum ShapeType { Triangle, Rectangle, Square };
+    class ShapeFactory
+    {
+        public static Shape CreateShape(ShapeType shape,Vector2[] dot)
+        {
+            switch (shape)
+            {
+                case ShapeType.Triangle:
+                    return new Triangle(dot[0],dot[1],dot[2]);
+                case ShapeType.Rectangle:
+                    return new Rectangle(dot[0], dot[1]);
+                case ShapeType.Square:
+                    return new Square(dot[0], dot[1]);
+                default:
+                    return null;
+            }
+        }
+
+        public static void GetRandomDots(Vector2[] dots)
+        {
+            Random rd = new Random(Guid.NewGuid().GetHashCode());
+            for (int i = 0; i < dots.Length; i++)
+            {
+                dots[i].X = rd.Next(1, 30);
+                dots[i].Y = rd.Next(1, 30);
+            }
+        }
+
+        public static List<Shape> GetRandomShapes(int n)
+        {
+            Random rd = new Random(Guid.NewGuid().GetHashCode());
+            List<Shape> shapes = new List<Shape>();
+            for (int i = 0; i < n; i++)
+            {
+                int type = rd.Next(1, 4);
+                switch (type)
+                {
+                    case 1:
+                        Vector2[] dots1 = new Vector2[3];
+                        GetRandomDots(dots1);
+                        shapes.Add(CreateShape(ShapeType.Triangle, dots1));
+                        break;
+                    case 2:
+                        Vector2[] dots2 = new Vector2[2];
+                        GetRandomDots(dots2);
+                        shapes.Add(CreateShape(ShapeType.Rectangle, dots2));
+                        break;
+                    case 3:
+                        Vector2[] dots3 = new Vector2[2];
+                        GetRandomDots(dots3);
+                        shapes.Add(CreateShape(ShapeType.Square, dots3));
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return shapes;
+        }
+
+        public static void DisplayShape(List<Shape> shapes)
+        {
+            float sum = 0;
+            foreach(Shape x in shapes)
+            {
+                if (x.IsLegal())
+                {
+                    Console.WriteLine($"Type:{x.GetType()} Area:{x.Area}");
+                    sum += x.Area;
+                }
+            }
+            Console.WriteLine($"AreaSum:{sum}");
+        }
+
+        public static void Main(string[] args)
+        {
+            /* 
+             * 此程序通过2或3个点来生成随机图形
+             * 当这2个3个点不符合相关图形定义（如：使得正方形长宽不相等）时，该图形将不会被输出至Console
+             * 其面积也不会参与最后的总面积计算
+             * 基于以上原因，该程序将会较少地生成正方形(Square)，因其合法的条件较为苛刻
+             */
+            try
+            {
+                List<Shape> shapes = GetRandomShapes(10);
+                DisplayShape(shapes);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("error:{0}", e.Message);
+            }
+        }
+    }
+}
