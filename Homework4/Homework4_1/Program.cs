846939fbd7a5f3904132e174f6cb4e54c99e5d5e
renranxie <renranxie@hotmail.com>
2021-03-19 10:12:20 +0800

    Commit Homework4

---
+++ Homework4/Homework4_1/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace Homework4_1
+{
+    public class MyList<T>
+    {
+        public class Node<_T>
+        {
+            public Node<_T> Next { get; set; }
+            public _T Data { get; set; }
+
+            public Node(_T t)
+            {
+                Next = null;
+                Data = t;
+            }
+        }
+
+        private Node<T> head;
+        private Node<T> tail;
+
+        public Node<T> Head { get => head; }
+        public Node<T> Tail { get => tail; }
+
+        public MyList()
+        {
+            head = tail = null;
+        }
+
+        public void Add(T t)
+        {
+            Node<T> node = new Node<T>(t);
+            if (tail == null)
+                head = tail = node;
+            else
+            {
+                tail.Next = node;
+                tail = node;
+            }
+        }
+
+        public void ForEach(Action<T> action)
+        {
+            try
+            {
+                Node<T> p = Head;
+                while (p.Next != null)
+                {
+                    action(p.Data);
+                    p = p.Next;
+                }
+                action(Tail.Data);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine($"error:{e.Message}");
+            }
+        }
+
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> myList = new MyList<int>();
+            for (int i = 0; i < 10; i++)
+                myList.Add(i);
+
+            Action<int> print = n => Console.Write($"{n} ");
+            myList.ForEach(print);
+            Console.Write('\n');
+
+            int max = Int32.MinValue;
+            Action<int> getMax = n => { if (n > max) max = n; };
+            myList.ForEach(getMax);
+
+            int min = Int32.MaxValue;
+            Action<int> getMin = n => { if (n < min) min = n; };
+            myList.ForEach(getMin);
+
+            int sum = 0;
+            Action<int> getSum = n => sum += n;
+            myList.ForEach(getSum);
+
+            Console.WriteLine($"Max:{max} Min:{min} Sum:{sum}");
+        }
+    }
+}
---
+++ Homework4/Homework4_2/Clock.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Threading;
+
+namespace Homework4_2
+{
+    public struct TimeArgs
+    {
+        public int Hour{ get; set; }
+        public int Minute { get; set; }
+        public int Second { get; set; }
+
+        public static bool operator==(TimeArgs r,TimeArgs l)
+        {
+            if (r.Hour == l.Hour && r.Minute == l.Minute && r.Second == l.Second)
+                return true;
+            else
+                return false;
+        }
+        public static bool operator !=(TimeArgs r, TimeArgs l)
+        {
+            if (r.Hour == l.Hour && r.Minute == l.Minute && r.Second == l.Second)
+                return false;
+            else
+                return true;
+        }
+    }
+
+    public delegate void ClockHandler(object sender, TimeArgs time);
+
+    public class Clock
+    {
+        public TimeArgs time;
+        public TimeArgs alarmTime;
+
+        public event ClockHandler OnTick;
+        public event ClockHandler OnAlarm;
+
+        public Clock(int hour,int minute)
+        {
+            try
+            {
+                if (hour < 24 && hour >= 0 && minute < 60 && minute >= 0)
+                {
+                    time.Hour = hour;
+                    time.Minute = minute;
+                    time.Second = 0;
+                }
+                else
+                    throw new Exception("Input time occurs error");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine($"error:{e.Message}");
+            }
+        }
+
+        public void SetClock(int hour, int minute)
+        {
+            try
+            {
+                if (hour < 24 && hour >= 0 && minute < 60 && minute >= 0)
+                {
+                    time.Hour = hour;
+                    time.Minute = minute;
+                    time.Second = 0;
+                }
+                else
+                    throw new Exception("Input time occurs error");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"error:{e.Message}");
+            }
+        }
+
+        public void SetAlarm(int hour, int minute)
+        {
+            try
+            {
+                if (hour < 24 && hour >= 0 && minute < 60 && minute >= 0)
+                {
+                    alarmTime.Hour = hour;
+                    alarmTime.Minute = minute;
+                    alarmTime.Second = 0;
+                }
+                else
+                    throw new Exception("Input time occurs error");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"error:{e.Message}");
+            }
+        }
+
+        public void RunStep()
+        {
+            if (time.Second < 59)
+                time.Second++;
+            else
+            {
+                time.Second = 0;
+                time.Minute++;
+                if (time.Minute > 59)
+                {
+                    time.Minute = 0;
+                    time.Hour = (time.Hour + 1) % 24;
+                }
+            }
+            if (time == alarmTime)
+            {
+                OnAlarm(this, time);
+                OnTick(this, time);
+            } 
+            else
+                OnTick(this, time);
+            Thread.Sleep(1000);
+        }
+
+        public void Run()
+        {
+            while (true)
+            {
+                RunStep();
+            }
+        }
+    }
+
+    class MyClock
+    {
+        public static void OnTick1(object sender,TimeArgs time)
+        {
+            Console.WriteLine($"{time.Hour}:{time.Minute}:{time.Second}");
+        }
+        public static void OnAlarm1(object sender, TimeArgs time)
+        {
+            Console.WriteLine("Ding!");
+        }
+        static void Main(string[] args)
+        {
+            Clock clock = new Clock(0, 0);
+            clock.SetAlarm(0, 1);
+            clock.OnTick += OnTick1;
+            clock.OnAlarm += OnAlarm1;
+            clock.Run();
+        }
+    }
+}
